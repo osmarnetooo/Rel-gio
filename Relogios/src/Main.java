47cbba410959fe10a5fa5561df0a5781aef59791
Osmar Neto <osmar.para.trabalho@gmail.com>
2025-03-15 01:33:40 -0300

    Codigo Revisado

--- Relogios/src/Main.java
+++ Relogios/src/Main.java
@@ -1,14 +1,14 @@
-
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-         Clock brlclock = new BRLClock();
-         brlclock.setSecond(00);
-         brlclock.setMinute(00);
-         brlclock.setHour(25);
 
-         System.out.println(brlclock.getTime());
-         System.out.println(new USClock().convert(brlclock).getTime());
+    public static void main(String[] args) throws Exception {
+        Clock brlclock = new BRLClock();
+        brlclock.setSecond(30);
+        brlclock.setMinute(10);
+        brlclock.setHour(16);
+
+        System.out.println(brlclock.getTime());
+        System.out.println(new USClock().convert(brlclock).getTime());
+
+        
     }
 }
--- Relogios/src/USClock.java
+++ Relogios/src/USClock.java
@@ -19,7 +19,7 @@ public non-sealed class USClock extends Clock {
         setBeforeMidday();
         if((hour > 12) && (hour <= 23)){
             setAfterMidday();
-            this.hour -= 12;
+            this.hour = hour - 12;
             }else if(hour >= 24){
                 this.hour = 0;
             }else{
